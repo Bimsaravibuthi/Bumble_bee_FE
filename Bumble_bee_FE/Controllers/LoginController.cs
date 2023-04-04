ba691514630634e1eba87ec33557ecb2a09e02d6
Bimsara Vibuthi Senarath <bimsaravibuthi1541@gmail.com>
2023-04-04 10:13:49 +0530

    Add project files.

---
+++ Bumble_bee_FE/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Bumble_bee_FE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bumble_bee_FE.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Login login) 
+        {
+            using(var httpClient = new HttpClient())
+            {
+
+            }
+            return View();
+        }
+    }
+}
---
+++ Bumble_bee_FE/Models/Login.cs
@@ -0,0 +1,9 @@
+﻿namespace Bumble_bee_FE.Models
+{
+    public class Login
+    {
+        public string? UserEmail { get; set; }
+        public string? Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
